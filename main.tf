1b309716aab77f7c68a45ace974ae042f9c1431e
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-05-03 17:54:52 +0900

    add cloud schedule event

--- main.tf
+++ main.tf
@@ -14,7 +14,18 @@ provider "google" {
   region      = "asia-northeast1"
 }
 
-resource "google_pubsub_topic" "scheduled-topic" {
-  name    = "scheduled-topic"
+resource "google_pubsub_topic" "topic" {
+  name    = "rss-webhook-topic"
   project = "${var.project_id}"
+}
+
+resource "google_cloud_scheduler_job" "job" {
+  name        = "rss-webhook-job"
+  description = "rss webhook job"
+  schedule    = "0 */3 * * *"
+
+  pubsub_target {
+    topic_name = "${google_pubsub_topic.topic.id}"
+    data       = "${base64encode("{\"greeting\": \"hello\"}")}"
+  }
 }
